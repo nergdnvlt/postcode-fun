53d2f3cd8cf0945dc7a8bbd6266040823c1e1ba6
Tyler Lundgren <tylerlundgren@yahoo.com>
2020-02-08 14:25:26 -0700

    Adding test for trying to eval LS1 based cafes.

--- spec/models/cafe_spec.rb
+++ spec/models/cafe_spec.rb
@@ -13,10 +13,6 @@ RSpec.describe Cafe, type: :model do
 
   describe 'Instance Methods' do
     it 'correctly populates ls1 small category' do
-      cat = Category.create!(name: 'ls1 small')
-      Category.create!(name: 'ls1 medium')
-      Category.create!(name: 'ls1 large')
-
       post_code = PostalCode.create(code: 'LS1 5HU')
       cafe1 = post_code.cafes.create(
         name: "Fluffy's",
@@ -25,7 +21,6 @@ RSpec.describe Cafe, type: :model do
       )
 
       expect(cafe1.postal_code).to eq(post_code)
-      expect(cafe1.category).to eq(cat)
       expect(cafe1.category.name).to eq('ls1 small')
     end
   end
